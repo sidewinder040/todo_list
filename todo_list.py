6513fb596691e968f561139fb2b17638bab489c3
sidewinder040 <macevic040@hotmail.co.uk>
2018-05-24 21:13:16 +0100

    Items list to store ItemClass instances

--- todo_list.py
+++ todo_list.py
@@ -1,5 +1,16 @@
 from ItemClass import todo_item
 
-item1 = todo_item("Test", 3)
+items = [] # List to contain ToDo ItemClass instances
 
+# Test Item
+item1 = todo_item("Test", 3)
+items.append(item1)
+
+# Add another item
+item2 = todo_item("Test 2", 2)
+items.append(item2)
+
+print "---------------------"
+print "Items in list..."
 item1.Print_Item()
+item2.Print_Item()
